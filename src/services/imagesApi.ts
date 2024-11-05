a6a2fbea9d59151ab80ab3a9f08f9c3e94fcebb2
hktari <bkamnik1995@gmail.com>
2024-11-05 12:29:38 +0100

    configure api url environemnt variable and whitelist images.unsplash.com

--- src/services/imagesApi.ts
+++ src/services/imagesApi.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { Random } from "unsplash-js/dist/methods/photos/types";
 
-axios.defaults.baseURL = process.env.API_URL;
+console.log(process.env.NEXT_PUBLIC_API_URL);
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 const getImages = async (query: string, count?: number): Promise<Random[]> => {
   const response = await axios.get(`/images?query=${query}&count=${count}`);
   return response.data;
 };
-
 
 export { getImages };
