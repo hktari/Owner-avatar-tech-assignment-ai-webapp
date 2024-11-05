4cd9460c40acc3edaae732db02f555169cb65aa9
hktari <bkamnik1995@gmail.com>
2024-11-05 13:55:50 +0100

    remove console.log

--- src/services/imagesApi.ts
+++ src/services/imagesApi.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { Random } from "unsplash-js/dist/methods/photos/types";
 
-console.log(process.env.NEXT_PUBLIC_API_URL);
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 const getImages = async (query: string, count?: number): Promise<Random[]> => {
