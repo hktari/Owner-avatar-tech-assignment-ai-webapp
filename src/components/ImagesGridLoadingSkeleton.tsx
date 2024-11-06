cce9164c3cf9c1021c6fa27504e0b651a7bb2863
hktari <bkamnik1995@gmail.com>
2024-11-06 21:07:37 +0100

    edit layout of ImageCardLoadingSkeleton

--- src/components/ImagesGridLoadingSkeleton.tsx
+++ src/components/ImagesGridLoadingSkeleton.tsx
@@ -4,15 +4,11 @@ import Grid from './Grid'
 
 const ImageCardLoadingSkeleton = () => {
     return (
-        <div className="bg-gray-200 rounded-lg overflow-hidden shadow-md w-full max-w-[244px] h-72 animate-pulse">
+        <div className="bg-gray-200 rounded-lg overflow-hidden shadow-md w-full max-w-[244px] h-72 animate-pulse space-y-4 relative">
             <div className="h-40 bg-gray-300"></div>
-            <div className="p-4">
-            <div className="h-4 w-3/4 bg-gray-300 mb-2"></div>
-            <div className="h-4 w-1/2 bg-gray-300"></div>
-            </div>
-            <div className="p-4">
-            <div className="h-8 w-1/2 bg-gray-300"></div>
-            </div>
+            <div className="h-4 w-3/4 bg-gray-300 mb-2 mx-auto"></div>
+            <div className="h-8 w-1/2 bg-gray-300 absolute bottom-3 left-1/2 transform -translate-x-1/2"></div>
+
         </div>
     )
 }
