f0b1847e49bb3c0693e30811956a61e46b8e4147
hktari <bkamnik1995@gmail.com>
2024-11-06 20:41:09 +0100

    add animation

--- src/components/ImagesGridLoadingSkeleton.tsx
+++ src/components/ImagesGridLoadingSkeleton.tsx
@@ -4,14 +4,14 @@ import Grid from './Grid'
 
 const ImageCardLoadingSkeleton = () => {
     return (
-        <div className="bg-gray-200 rounded-lg overflow-hidden shadow-md w-full max-w-[244px] h-72">
+        <div className="bg-gray-200 rounded-lg overflow-hidden shadow-md w-full max-w-[244px] h-72 animate-pulse">
             <div className="h-40 bg-gray-300"></div>
             <div className="p-4">
-                <div className="h-4 w-3/4 bg-gray-300 mb-2"></div>
-                <div className="h-4 w-1/2 bg-gray-300"></div>
+            <div className="h-4 w-3/4 bg-gray-300 mb-2"></div>
+            <div className="h-4 w-1/2 bg-gray-300"></div>
             </div>
             <div className="p-4">
-                <div className="h-8 w-1/2 bg-gray-300"></div>
+            <div className="h-8 w-1/2 bg-gray-300"></div>
             </div>
         </div>
     )
